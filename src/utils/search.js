6d750069f61b6d2afb4e8378d85c22010843afba
taiki-sakashita <minashi.business@gmail.com>
2023-08-03 19:23:23 +0900

    作業途中のコード達に// eslint-disable-next-lineした

--- src/utils/search.js
+++ src/utils/search.js
@@ -1,17 +1,19 @@
 import { getPokemonType } from '../utils/pokemon';
+// eslint-disable-next-line
 import React, {useState} from 'react';
 
 
 
-export const Search = ( {pokemon} ) => {
+export const Search = ( {pokemon} ) => {// eslint-disable-next-line
   const [pokemonTypeURL, setPokemonTypeURL] = useState([]);
-
+// eslint-disable-next-line
   let resPokemonTypes = pokemon.types.map((v) => {
   let typesURL = v.type.url;
   return typesURL;
 });
-
+// eslint-disable-next-line
   const TypeSearch = async(data) => {
+// eslint-disable-next-line
     let _pokemonType = await Promise.all(
       data.map(async (pokemon) => {
         let pokemonTypeDetail = await getPokemonType(pokemon);
